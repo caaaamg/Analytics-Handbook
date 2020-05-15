74145cc7fc7374938c1a21bc503e522135675818
Cameron Garvie <cameron.garvie96@gmail.com>
2020-05-15 15:38:59 +0100

    Linear Progression Parsing passing data Plotting with matplotlib Linear progression applied with scikit-learn

---
+++ Linear Regression.py
@@ -0,0 +1,46 @@
+# required imports
+import requests
+import pandas as pd
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+# setting the urls for later use
+base_url = "https://raw.githubusercontent.com/statsbomb/open-data/master/data/"
+comp_url = base_url + "matches/{}/{}.json"
+match_url = base_url + "events/{}.json"
+
+
+def parse_data(competition_id, season_id):
+    matches = requests.get(url=comp_url.format(competition_id, season_id)).json()
+    match_ids = [m['match_id'] for m in matches]
+
+    all_events = []
+
+    for match_id in tqdm(match_ids):
+
+        events = requests.get(url=match_url.format(match_id)).json()
+
+        passes = [x for x in events if x['type']['name'] == "Pass"]
+
+        for a in passes:
+            attributes = {
+                "player_id": a['player']['id'],
+                "outcome": 0 if 'outcome' in a['pass'].keys() else 1,
+            }
+            all_events.append(attributes)
+
+    return pd.DataFrame(all_events)
+
+
+competition_id = 43
+season_id = 3
+
+df = parse_data(competition_id, season_id)
+
+print(df.head(15))
+
+total_passes = df.groupby('player_id')['outcome'].sum()
+percentage = df.groupby('player_id')['outcome'].mean()
+
+plt.scatter(total_passes, percentage, alpha=0.8)
+plt.show()
