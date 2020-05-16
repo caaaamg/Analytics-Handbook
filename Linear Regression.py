0089620ce768675a27fcb4a43ba0c1f543691f04
Cameron Garvie <cameron.garvie96@gmail.com>
2020-05-16 04:40:35 +0100

    Linear Progression Comments and tidying up

--- Linear Regression.py
+++ Linear Regression.py
@@ -3,11 +3,14 @@ import requests
 import pandas as pd
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 # setting the urls for later use
 base_url = "https://raw.githubusercontent.com/statsbomb/open-data/master/data/"
 comp_url = base_url + "matches/{}/{}.json"
 match_url = base_url + "events/{}.json"
+
+# function to parse data into pandas DataFrame
 
 
 def parse_data(competition_id, season_id):
@@ -32,15 +35,24 @@ def parse_data(competition_id, season_id):
     return pd.DataFrame(all_events)
 
 
-competition_id = 43
-season_id = 3
+comp_id = 43
+seas_id = 3
 
-df = parse_data(competition_id, season_id)
+df = parse_data(comp_id, seas_id)
 
 print(df.head(15))
 
 total_passes = df.groupby('player_id')['outcome'].sum()
 percentage = df.groupby('player_id')['outcome'].mean()
 
+model = LinearRegression()
+fit = model.fit([[x] for x in total_passes], percentage)
+print("Coefficients: {}:".format(fit.coef_))
+print("Intercept: {}:".format(fit.intercept_))
+
+xfit = [0,500]
+yfit = model.predict([[x] for x in xfit])
+
 plt.scatter(total_passes, percentage, alpha=0.8)
+plt.plot(xfit,yfit, 'r')
 plt.show()
